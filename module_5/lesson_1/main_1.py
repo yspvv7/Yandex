0bf810400959e2af73fd0485cba58987a5da90e6
yspvv7 <mirakhmadyusupov@gmail.com>
2024-12-17 21:16:10 +0500

    lesson_1 create

---
+++ module_5/lesson_1/main_1.py
@@ -0,0 +1,9 @@
+"""
+echo "# Yandex" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yspvv7/Yandex.git
+git push -u origin main
+"""
